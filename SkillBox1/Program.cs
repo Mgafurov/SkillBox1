45e8349e90fc2eabd62153280c10efee67c59e77
posey <posey@DESKTOP-KEAP1HJ>
2023-08-15 21:43:28 +0500

    Добавьте файлы проекта.

---
+++ SkillBox1/Program.cs
@@ -0,0 +1,23 @@
+﻿/*using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World!!!");
+        Console.ReadKey();
+    }
+}*/
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Hello");
+        Console.Write(" World");
+        Console.Write(" !!!");
+        Console.ReadKey();
+    }
+}
